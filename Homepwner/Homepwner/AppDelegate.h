10fb2b366f229d338d87e080fa7c72a988528140
charlieusc <yangjialinusc@gmail.com>
2016-08-04 11:58:21 -0700

    initial project

---
+++ Homepwner/Homepwner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homepwner
+//
+//  Created by YangJialin on 8/4/16.
+//  Copyright © 2016 YangJialin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Homepwner/Homepwner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Homepwner
+//
+//  Created by YangJialin on 8/4/16.
+//  Copyright © 2016 YangJialin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
