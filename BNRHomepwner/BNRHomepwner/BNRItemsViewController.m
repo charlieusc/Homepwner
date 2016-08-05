440a815bf40af2c062e026cab6d51616cbff0e5b
charlieusc <yangjialinusc@gmail.com>
2016-08-05 15:05:39 -0700

    add no more items! at last cell

--- BNRHomepwner/BNRHomepwner/BNRItemsViewController.m
+++ BNRHomepwner/BNRHomepwner/BNRItemsViewController.m
@@ -38,21 +38,33 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    if(section==0) return [[[BNRItemStore sharedStore] allCheapItems] count];
-    return [[[BNRItemStore sharedStore] allExpenItems] count];
+    if(section==0) return [[[BNRItemStore sharedStore] allCheapItems] count]+1;
+    return [[[BNRItemStore sharedStore] allExpenItems] count]+1;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
     NSArray *items;
+    NSInteger index;
     if(indexPath.section == 0){
         items = [[BNRItemStore sharedStore] allCheapItems];
+        if(indexPath.row == [[[BNRItemStore sharedStore] allCheapItems] count]){
+            cell.textLabel.text = @"no more items!";
+        }else{
+            BNRItem *item = items[indexPath.row];
+            cell.textLabel.text = [item description];
+        }
     }else{
         items = [[BNRItemStore sharedStore] allExpenItems];
+        if(indexPath.row == [[[BNRItemStore sharedStore] allExpenItems] count]){
+            cell.textLabel.text = @"no more items!";
+        }else{
+            BNRItem *item = items[indexPath.row];
+            cell.textLabel.text = [item description];
+        }
     }
-    BNRItem *item = items[indexPath.row];
-    cell.textLabel.text = [item description];
+    
     return cell;
 }
 
