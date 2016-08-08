2e10911b351f1db1f670b41fd5a6ac64b91845cd
charlieusc <yangjialinusc@gmail.com>
2016-08-08 12:20:38 -0700

    challenge_9 done, added last cell and customized it so that it can not move.

--- BNRHomepwner/BNRHomepwner/BNRItemsViewController.m
+++ BNRHomepwner/BNRHomepwner/BNRItemsViewController.m
@@ -35,17 +35,23 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return [[[BNRItemStore sharedStore] allItems] count];
+    return [[[BNRItemStore sharedStore] allItems] count] +1;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
     
-    NSArray *items = [[BNRItemStore sharedStore] allItems];
-    BNRItem *item = items[indexPath.row];
     
-    cell.textLabel.text = [item description];
+    NSArray *items;
+    BNRItem *item;
+    items = [[BNRItemStore sharedStore] allItems];
+    if(indexPath.row == [[[BNRItemStore sharedStore] allItems] count]){
+        cell.textLabel.text = @"no more items!";
+    }else{
+        BNRItem *item = items[indexPath.row];
+        cell.textLabel.text = [item description];
+    }
     
     return cell;
 }
@@ -88,6 +94,10 @@
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    if(indexPath.row == [[[BNRItemStore sharedStore] allItems] count]){
+        return;
+    }
+    
     if(editingStyle == UITableViewCellEditingStyleDelete){
         NSArray *items = [[BNRItemStore sharedStore] allItems];
         BNRItem *item = items[indexPath.row];
@@ -103,7 +113,24 @@
 
 - (NSString *)tableView:(UITableView *)tableView titleForDeleteConfirmationButtonForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    NSInteger row = [[[BNRItemStore sharedStore] allItems] count];
+    if(indexPath.row == row){
+        return @"";
+    }
     return @"Remove";
+}
+
+//This method allows customization of the target row for a particular row as it is being moved up and down a table view. As the dragged row hovers over another row, the destination row slides downward to visually make room for the relocation; this is the location identified by proposedDestinationIndexPath.
+- (NSIndexPath *)tableView:(UITableView *)tableView
+targetIndexPathForMoveFromRowAtIndexPath:(NSIndexPath *)sourceIndexPath
+       toProposedIndexPath:(NSIndexPath *)proposedDestinationIndexPath
+{
+    NSInteger row = [[[BNRItemStore sharedStore] allItems] count];
+    if(sourceIndexPath.row == row || proposedDestinationIndexPath.row == row){
+        return sourceIndexPath;
+    }else{
+        return proposedDestinationIndexPath;
+    }
 }
 
 
